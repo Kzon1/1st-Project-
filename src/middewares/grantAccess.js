054ee829d67be5bccc22d37eb2d585980cd1f8ac
your name <pastover143@gmail.com>
2024-03-25 01:02:01 +0700

    update backend

---
+++ src/middewares/grantAccess.js
@@ -0,0 +1,16 @@
+
+
+
+const grantAccess = (action, resource) => {
+  return async (req, res, next) => {
+    const permission = roles.can(req.payload.role)[action](resource);
+
+    if (!permission.granted) {
+      return next(Boom.unauthorized("You don't have permission."));
+    }
+
+    next();
+  };
+};
+
+export default grantAccess;
